d77a46619100970402d4542a30cbf386d6a58e74
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2024-11-22 10:23:17 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./page.module.css";
+import HomeContent from "./Components/homeContent";
+
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <HomeContent />
+    
+    </div>
+  );
+}
